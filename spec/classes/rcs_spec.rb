8cc9ffdc2264c9a9881a2bfa0d93dd16f2369e48
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2015-06-23 20:43:39 +0100

    Ensure => file, not present

--- spec/classes/rcs_spec.rb
+++ spec/classes/rcs_spec.rb
@@ -2,6 +2,6 @@ require_relative '../spec_helper'
 
 describe 'rcs::config', :type => :class do
 
-  it { should contain_file('/etc/default/rcS').with_ensure('present') }
+  it { should contain_file('/etc/default/rcS').with_ensure('file') }
 
 end
